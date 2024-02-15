3c4439ed446331e9282be11da614f8a2d44d951a
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2024-02-15 17:12:25 +0530

    node mailer updated

--- functions/mailer.js
+++ functions/mailer.js
@@ -2,13 +2,27 @@ export async function sendMail(mailerData) {
   const { subject, sender, receivers, textContent, htmlContent } = mailerData;
   // console.log("mailer Data", mailerData);
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service: "outlook",
+  // let transporter = nodemailer.createTransport({
+  //   host: "smtp.outlook.com",
+  //   service: "outlook",
+  //   secure: false,
+  //   auth: {
+  //     user: process.env.EMAIL,
+  //     pass: process.env.PASS,
+  //   },
+  // });
+  var transporter = nodemailer.createTransport("SMTP", {
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: false, 
+    port: 587, 
     auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASS,
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
     },
-  });
+    tls: {
+        ciphers:'SSLv3'
+    }
+});
   // send mail with defined transporter object
   let info = await transporter.sendMail({
     from: `${sender.name} <${sender.mailId}>`,
